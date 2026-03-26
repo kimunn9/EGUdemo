71e93e91ea0a04c51b7b5e3d4ef33a3da1db269c
t1o0k <takanen0810@gmail.com>
2026-03-27 02:43:58 +0900

    indexファイルとmember.jsの追加

---
+++ js/member.js
@@ -0,0 +1,14 @@
+const membersData = [
+  {
+    id: 1,
+    name: "トッサノ",
+    tag: ["主催"],
+    color: "#111111", // Black
+    desc: "EnjoyGameUnion!!の発起人。",
+    images: [
+      ""
+    ],
+  },
+];
+
+export default membersData;
