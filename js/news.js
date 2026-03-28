2112ed601c9b09caabb1bba0ac4e929c3e9246f8
t1o0k <takanen0810@gmail.com>
2026-03-29 00:20:40 +0900

    newsを外部ファイルから読み込むように調整

---
+++ js/news.js
@@ -0,0 +1,24 @@
+
+const newsContent = [
+    {
+        id: 1,
+        title: "EnjoyGameUnion!! 公式Webサイトをオープンしました！",
+        date: "2026.03.20",
+        tag: "お知らせ",
+        body: `<p class='text-slate-600 mb-8 leading-loose text-lg'>オープンってワケ</p>`
+    },
+    {
+        id: 2,
+        title: "新曲「Enjoy Virtual Life」MV公開！",
+        date: "2026.03.20",
+        tag: "動画更新",
+        body: `<p class='text-slate-600 mb-8 leading-loose text-lg'>Union全メンバーが参加した初の合唱曲がリリース。34名の個性が重なり合うハーモニーをぜひお楽しみください。</p>`
+    },
+    {
+        id: 3,
+        title: "VRChat内「春の大型FPS大会」にメンバー3名が出場決定！",
+        date: "2026.03.25",
+        tag: "活動報告",
+        body: `<p class='text-slate-600 mb-8 leading-loose text-lg'>来月開催されるVRChat最大級のFPSトーナメントに、Unionから選抜チームが出場します。練習の様子はYouTubeでも公開中！</p>`
+    },
+];
