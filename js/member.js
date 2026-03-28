fa97479693f83e9f66939edd4fd976c932aa1eed
t1o0k <takanen0810@gmail.com>
2026-03-28 20:42:10 +0900

    メンバーリストを外部ファイルから読み込むように調整

--- js/member.js
+++ js/member.js
@@ -2,13 +2,378 @@ const membersData = [
   {
     id: 1,
     name: "トッサノ",
+    vrc: "トッサノ",
     tag: ["主催"],
-    color: "#111111", // Black
+    color: "#111111",
     desc: "EnjoyGameUnion!!の発起人。",
     images: [
       ""
     ],
+    x: "tossanovrc",
+    youtube: "",
+  },
+  {
+    id: 2,
+    name: "ずんだゆき",
+    vrc: "ずんだゆき",
+    tag: ["運営・裏方"],
+    color: "#5ED464",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "zund_Yuki",
+    youtube: "",
+  },
+  {
+    id: 3,
+    name: "sava",
+    vrc: "sava_403",
+    tag: ["運営・裏方"],
+    color: "#7148B0",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "Sava_403",
+    youtube: "",
+  },
+  {
+    id: 4,
+    name: "Dr.Anri",
+    vrc: "Dr_Anri",
+    tag: ["メンバー"],
+    color: "#FF6FFF",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "Es96L4ap",
+    youtube: "",
+  },
+  {
+    id: 5,
+    name: "まじぇんて",
+    vrc: "majennte",
+    tag: ["メンバー"],
+    color: "#C77300",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "majennte_vrc",
+    youtube: "",
+  },
+  {
+    id: 6,
+    name: "UTAN",
+    vrc: "UTAN_DX",
+    tag: ["メンバー"],
+    color: "#fc2915",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "UTANdayote",
+    youtube: "",
+  },
+  {
+    id: 7,
+    name: "エリリ・エリ",
+    vrc: "エリリ・エリ",
+    tag: ["メンバー"],
+    color: "#ffef0c",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "nameSnow66",
+    youtube: "",
+  },
+  {
+    id: 8,
+    name: "garavella",
+    vrc: "garavella",
+    tag: ["メンバー"],
+    color: "#900ffa",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "garavella1010",
+    youtube: "",
+  },
+  {
+    id: 9,
+    name: "白夏ハル",
+    vrc: "Shiranatsu_haru",
+    tag: ["メンバー"],
+    color: "#ff9dff",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "shiranatsu_haru",
+    youtube: "",
+  },
+  {
+    id: 10,
+    name: "NAOZEN",
+    vrc: "NAOZEN s8",
+    tag: ["メンバー"],
+    color: "#ff7b00",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "NAOZEN1110",
+    youtube: "",
+  },
+  {
+    id: 11,
+    name: "ぐらすず",
+    vrc: "ぐらすず",
+    tag: ["メンバー"],
+    color: "#00CD2F",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "gura_ow2",
+    youtube: "",
+  },
+  {
+    id: 12,
+    name: "もっく",
+    vrc: "もっくたろう。",
+    tag: ["メンバー"],
+    color: "#f51e0f",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "eiheihei_dk",
+    youtube: "",
+  },
+  {
+    id: 13,
+    name: "しんやぞ",
+    vrc: "しんやぞ",
+    tag: ["メンバー"],
+    color: "#ffd643",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "Zz_sinsin_Zz",
+    youtube: "",
+  },
+  {
+    id: 14,
+    name: "saboint",
+    vrc: "saboint",
+    tag: ["メンバー"],
+    color: "#ee8a2c",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "morrimorriGhh",
+    youtube: "",
+  },
+  {
+    id: 15,
+    name: "suraimi",
+    vrc: "metal suraimi",
+    tag: ["メンバー"],
+    color: "#2B8EFF",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "siawase_suraimi",
+    youtube: "",
+  },
+  {
+    id: 16,
+    name: "おねずみとと",
+    vrc: "とと（toto）",
+    tag: ["メンバー"],
+    color: "#ffb8f0",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "Tototo_Tatin",
+    youtube: "",
+  },
+  {
+    id: 17,
+    name: "ちぇるびん",
+    vrc: "ちぇるびん",
+    tag: ["メンバー"],
+    color: "#7c11a7",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "chelb1n",
+    youtube: "",
+  },
+  {
+    id: 18,
+    name: "こまつちま",
+    vrc: "komatsu_",
+    tag: ["メンバー"],
+    color: "#D9F2FF",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "komatsu_chiii",
+    youtube: "",
+  },
+  {
+    id: 19,
+    name: "永夏",
+    vrc: "Tooka_",
+    tag: ["メンバー"],
+    color: "#4e1ff8",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "t1o0k",
+    youtube: "",
+  },
+  {
+    id: 20,
+    name: "巡璃",
+    vrc: "rneguri",
+    tag: ["メンバー"],
+    color: "#d1d1d1",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "rneguri",
+    youtube: "",
+  },
+  {
+    id: 21,
+    name: "Chelsea",
+    vrc: "ChelseaPOP",
+    tag: ["メンバー"],
+    color: "#eb5a96",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "ManyaruYazawa",
+    youtube: "",
+  },
+  {
+    id: 22,
+    name: "RIN",
+    vrc: "RI------------N",
+    tag: ["メンバー"],
+    color: "#f1ccb7",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "",
+    youtube: "",
+  },
+  {
+    id: 23,
+    name: "ふなナッツ",
+    vrc: "ふなナッツ",
+    tag: ["メンバー"],
+    color: "#bb895a",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "_funa_27",
+    youtube: "",
+  },
+  {
+    id: 24,
+    name: "スフレ・クルル",
+    vrc: "すふれぇ",
+    tag: ["メンバー"],
+    color: "#cacaca",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "sufure378481",
+    youtube: "",
+  },
+  {
+    id: 25,
+    name: "るた",
+    vrc: "るた／ruta",
+    tag: ["メンバー"],
+    color: "#e99481",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "ruta_xa",
+    youtube: "",
+  },
+  {
+    id: 26,
+    name: "KAITO",
+    vrc: "KAITO-030",
+    tag: ["メンバー"],
+    color: "#205ada",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "",
+    youtube: "",
+  },
+  {
+    id: 27,
+    name: "61",
+    vrc: "61__",
+    tag: ["メンバー"],
+    color: "#b80909",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "",
+    youtube: "",
+  },
+  {
+    id: 28,
+    name: "しの",
+    vrc: "sino_214",
+    tag: ["メンバー"],
+    color: "#424242",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "yukino_exe",
+    youtube: "",
+  },
+  {
+    id: 29,
+    name: "mikanoisii",
+    vrc: "mikanoisii",
+    tag: ["メンバー"],
+    color: "#8f67b6",
+    desc: "",
+    images: [
+      ""
+    ],
+    x: "mikanoisii72360",
+    youtube: "",
   },
 ];
-
-export default membersData;
